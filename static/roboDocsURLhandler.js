56f5491bd2c512edf0f28ac2647f9d3e1ba28eae
Frederik Berlaen <frederik@typemytype.com>
2014-12-24 14:50:04 +0100

    adding a url handler

---
+++ static/roboDocsURLhandler.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".roboFontLink").each(function () {
+        this.protocol = "robofont-script";
+    });
+    $(".drawBotlink").each(function () {
+        this.protocol = "drawbot";
+    });
+});
